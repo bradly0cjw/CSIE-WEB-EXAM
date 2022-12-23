407e523355358b5502f3c7ea603624ed6cd4923e
bradly0cjw <bradly093@gmail.com>
2022-12-24 04:39:03 +0800

    auto format

--- src/dynamicbackground.js
+++ src/dynamicbackground.js
@@ -1,22 +1,21 @@
 var x = setInterval(function () {
-var nowtime = new Date().getTime();
-var hours = (Math.floor((nowtime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))) * (1);
-var background = document.getElementById("hero");
-if(hours>=6 && hours<16 ){
-    background.classList.add("day");
-    background.classList.remove("evening");
-    background.classList.remove("night");
-    clearInterval(x);
-
-}else if(hours>=16 && hours<19 ){
-    background.classList.remove("day");
-    background.classList.add("evening");
-    background.classList.remove("night");
-    clearInterval(x);
-}else if(hours<16 || hours>=19 ){
-    background.classList.remove("day");
-    background.classList.remove("evening");
-    background.classList.add("night");
-    clearInterval(x);
-}
+    var nowtime = new Date().getTime();
+    var hours = (Math.floor((nowtime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))) * (1);
+    var background = document.getElementById("hero");
+    if (hours >= 6 && hours < 16) {
+        background.classList.add("day");
+        background.classList.remove("evening");
+        background.classList.remove("night");
+        clearInterval(x);
+    } else if (hours >= 16 && hours < 19) {
+        background.classList.remove("day");
+        background.classList.add("evening");
+        background.classList.remove("night");
+        clearInterval(x);
+    } else if (hours < 16 || hours >= 19) {
+        background.classList.remove("day");
+        background.classList.remove("evening");
+        background.classList.add("night");
+        clearInterval(x);
+    }
 }, 1000);
