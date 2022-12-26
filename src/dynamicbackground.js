a13eda70aaf378faae75dfc8718c8d6a588ba57a
bradly0cjw <bradly093@gmail.com>
2022-12-26 20:21:47 +0800

    fix js error

--- src/dynamicbackground.js
+++ src/dynamicbackground.js
@@ -1,16 +1,18 @@
 var x = setInterval(function () {
-var nowtime = new Date().getTime();
-var hours = (Math.floor((nowtime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))) * (1);
+var nowtime = new Date().getHours();
+// var hours = (Math.floor((nowtime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))) * (1);
+var hours=parseInt(nowtime)
 var background = document.getElementById("hero");
-if(hours>=6 || hours<16 ){
+console.log(hours)
+if(hours>=6 && hours<16 ){
     background.classList.add("day");
     background.classList.remove("evening");
     background.classList.remove("night");
-}else if(hours>=16 || hours<19 ){
+}else if(hours>=16 && hours<19 ){
     background.classList.remove("day");
     background.classList.add("evening");
     background.classList.remove("night");
-}else if(hours<16 || hours>=19 ){
+}else if(hours<6 || hours>=19 ){
     background.classList.remove("day");
     background.classList.remove("evening");
     background.classList.add("night");
