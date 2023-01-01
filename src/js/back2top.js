b44c1f3bd2f62751679532ffb530897c484570bf
bradly0cjw <bradly093@gmail.com>
2023-01-01 09:54:28 +0800

    optimizze

--- src/js/back2top.js
+++ src/js/back2top.js
@@ -1,7 +1,10 @@
 window.onload = function () {
   a = document.getElementById("btn")
-  window.onscroll = function () { scrollFunction() };
-
+  b = document.getElementById("down")
+  if (a != null) {
+    window.onscroll = function () { scrollFunction() };
+    a.onclick = scrolltotop;
+  }
   function scrollFunction() {
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
       a.classList.add("show");
@@ -9,9 +12,15 @@ window.onload = function () {
       a.classList.remove("show");
     }
   }
-  a.onclick = scrolltotop;
   function scrolltotop() {
     console.log("top")
     window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+  if (b != null) {
+    b.onclick = sdown;
+  }
+  function sdown() {
+    var heigh = window.innerHeight
+    window.scrollTo({ top: heigh, behavior: 'smooth' });
   }
 }
